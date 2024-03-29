a9a2044dd2e21cab6270d05b1bb1d72ba34d9ef2
Lucifer <mat.eduardo.arias@outlook.com>
2024-03-29 13:57:27 -0500

    feat (django_projects:bookdetection/urls.py) Actualiza importaciones para incluir nuevas vistas, corrige la ruta de add-book-description para usar BookDescriptionView y añade rutas para BookPDFProcessView y ExtractPDFInfoView, ampliando así la funcionalidad de la API para procesamiento y análisis de libros en formatos de imagen y PDF.

--- django_projects/bookdetection/urls.py
+++ django_projects/bookdetection/urls.py
@@ -2,11 +2,13 @@
 # Date: 29/02/2024
 
 from django.urls import path
-from .views import BookImageProcessView, BookDescriptionView
+from .views import BookImageProcessView, BookDescriptionView, BookPDFProcessView, ExtractPDFInfoView
 
 app_name = 'bookdetection'
 
 urlpatterns = [
     path('process-book-image/', BookImageProcessView.as_view(), name='process_book_image'),
-    path('add-book-description/', BookImageProcessView.as_view(), name='add_book_description'),
+    path('add-book-description/', BookDescriptionView.as_view(), name='add_book_description'),
+    path('process-book-pdf/', BookPDFProcessView.as_view(), name='process_book_pdf'),
+    path('extract-pdf-info/', ExtractPDFInfoView.as_view(), name='extract_pdf_info'),
 ]
