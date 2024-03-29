886a126bcaf4392f528d21ef0452c40357ecc9b6
Lucifer <mat.eduardo.arias@outlook.com>
2024-03-29 13:50:18 -0500

    feat (django_projects:bookdetection/models.py) Añade campos para gestionar libros digitales y físicos, incluyendo rutas a archivos y portadas

--- django_projects/bookdetection/models.py
+++ django_projects/bookdetection/models.py
@@ -12,9 +12,18 @@ class Genre(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=255, unique=True)
     author = models.CharField(max_length=255, blank=True, null=True)
-    image_path = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
+
+    digital_file_path = models.CharField(max_length=255, blank=True, null=True, help_text="Ruta al archivo digital del libro si está disponible.")
+
+    cover_image_path = models.CharField(max_length=255, blank=True, null=True, help_text="Ruta a la imagen de la portada del libro físico.")
+
+    TYPE_CHOICES = (
+        ('physical', 'Physical'),
+        ('digital', 'Digital'),
+    )
+    book_type = models.CharField(max_length=8, choices=TYPE_CHOICES, default='physical', help_text="Tipo de libro: físico o digital.")
 
     def __str__(self):
         return self.title
