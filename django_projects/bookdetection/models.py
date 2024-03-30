71a5fa4fee8beae56806095b368337fbffb16870
Lucifer <mat.eduardo.arias@outlook.com>
2024-03-29 20:22:19 -0500

    refactor (bookdetection/models.py) Incorpora método update_from_analysis en el modelo Book para centralizar la actualización de datos basada en análisis, simplifica la definición de book_type y mejora la gestión de temas relacionados

--- django_projects/bookdetection/models.py
+++ django_projects/bookdetection/models.py
@@ -21,18 +21,33 @@ class Book(models.Model):
     main_topic = models.ForeignKey(Genre, related_name='main_topic_books', on_delete=models.SET_NULL, null=True)
     secondary_topics = models.ManyToManyField(Topic, through='BookTopic')
     created_at = models.DateTimeField(auto_now_add=True)
-    
     digital_file_path = models.CharField(max_length=255, blank=True, null=True, help_text="Ruta al archivo digital del libro si está disponible.")
     cover_image_path = models.CharField(max_length=255, blank=True, null=True, help_text="Ruta a la imagen de la portada del libro físico.")
-
-    TYPE_CHOICES = (
-        ('physical', 'Physical'),
-        ('digital', 'Digital'),
-    )
-    book_type = models.CharField(max_length=8, choices=TYPE_CHOICES, default='physical', help_text="Tipo de libro: físico o digital.")
+    book_type = models.CharField(max_length=8, choices=[('physical', 'Physical'), ('digital', 'Digital')], default='physical', help_text="Tipo de libro: físico o digital.")
 
     def __str__(self):
         return self.title
+
+    def update_from_analysis(self, analysis_result):
+        self.title = analysis_result.get('title', self.title)
+        self.author = analysis_result.get('author', self.author)
+
+        main_topic_name = analysis_result.get('main_topic')
+        if main_topic_name:
+            main_topic, _ = Genre.objects.get_or_create(name=main_topic_name)
+            self.main_topic = main_topic
+
+        # Aquí se elimina la relación de temas secundarios existentes.
+        BookTopic.objects.filter(book=self).delete()
+
+        for idx, topic_name in enumerate(analysis_result.get('secondary_topics', [])):
+            topic, _ = Topic.objects.get_or_create(name=topic_name)
+            confidence_level = 'Low'  # Asume un nivel de confianza por defecto o ajusta como sea necesario.
+            # Si 'analysis_result' contiene niveles de confianza específicos, úsalos aquí.
+            BookTopic.objects.create(book=self, topic=topic, confidence_level=confidence_level)
+
+        self.save()
+
 
 class BookTopic(models.Model):
     book = models.ForeignKey(Book, related_name='book_topics', on_delete=models.CASCADE)
